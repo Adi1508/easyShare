32fc3d5c0ead4b8bc6f15fe230e9c79b915407e6
Adi1508 <adityak007mr@gmail.com>
2017-12-31 22:10:04 +0530

    Updates and Improvements

--- app/src/main/java/com/example/aditya/easyshare/MainActivity.java
+++ app/src/main/java/com/example/aditya/easyshare/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,8 +13,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
 import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
@@ -41,11 +50,16 @@ public class MainActivity extends AppCompatActivity {
     BufferedReader bufferedReader;
     StringBuilder stringBuilder;
     boolean check=true;
+    Uri uri;
+
+    private StorageReference mStorageRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mStorageRef = FirebaseStorage.getInstance().getReference();
 
         getImage = (Button)findViewById(R.id.display);
         uploadImage = (Button)findViewById(R.id.upload);
@@ -59,7 +73,15 @@ public class MainActivity extends AppCompatActivity {
                 intent.setType("image/*");
                 intent.setAction(Intent.ACTION_GET_CONTENT);
                 startActivityForResult(Intent.createChooser(intent,"Select Image From Gallery"), 1);
+            }
+        });
 
+        uploadImage.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                getImageFromEditText = getImageName.getText().toString();
+                uploadImageToServer();
             }
         });
     }
@@ -69,13 +91,43 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
 
         if(requestCode==1 && resultCode==RESULT_OK && data!=null && data.getData()!=null){
-            Uri uri=data.getData();
+            uri=data.getData();
             try{
+                System.out.println(uri);
                 fixBitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),uri);
                 showImage.setImageBitmap(fixBitmap);
             }catch(IOException e){
                 e.printStackTrace();
             }
         }
+    }
+
+    public void uploadImageToServer(){
+
+        StorageReference imageReference = mStorageRef.child(getImageFromEditText);
+        //StorageReference imageRef = mStorageRef.child("images/"+getImageFromEditText);
+
+        showImage.setDrawingCacheEnabled(true);
+        showImage.buildDrawingCache();
+        Bitmap bitmap= showImage.getDrawingCache();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] imageData=baos.toByteArray();
+
+        UploadTask uploadTask = imageReference.putBytes(imageData);
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>(){
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                System.out.println(downloadUrl);
+
+
+            }
+        });
     }
 }
